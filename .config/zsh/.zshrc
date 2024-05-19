dbe445e5d5caeabd66982a050e1f2e64f50478ec
loukwn <main@loukwn.me>
2024-05-19 22:20:20 +0100

    No title at startup of Kitty + title per command afterwards

--- .config/zsh/.zshrc
+++ .config/zsh/.zshrc
@@ -1,5 +1,9 @@
 # Initialization code that may require console input (password prompts, [y/n]
 # confirmations, etc.) must go above init_p10k; everything else may go below.
+
+# This just sets an empty title for terminals like Kitty and Foot.
+print -Pn "\e] \e\\"
+
 source "$ZSH_HOME"/init_p10k.zsh
 source "$ZSH_HOME"/zsh_config.zsh
 source "$ZSH_HOME"/functions.zsh
--- .config/zsh/functions.zsh
+++ .config/zsh/functions.zsh
@@ -1,11 +1,11 @@
-openall() {
-    # Open any given document file(s) for editing (or just viewing).
-    # Note1:
-    #    - Do not use for executable files!
-    # Note2:
-    #    - Uses 'mime' bindings, so you may need to use
-    #      e.g. a file manager to make proper file bindings.
+# Open any given document file(s) for editing (or just viewing).
+# Note1:
+#    - Do not use for executable files!
+# Note2:
+#    - Uses 'mime' bindings, so you may need to use
+#      e.g. a file manager to make proper file bindings.
 
+openall() {
     if [ -x /usr/bin/xdg-open ] ; then
         for file in "$@" ; do
             echo "xdg-open $file" >&2
@@ -15,4 +15,9 @@ openall() {
     fi
 
     echo "$FUNCNAME: package 'xdg-utils' or 'exo' is required." >&2
+}
+
+# This should update the title on Kitty and similar terminals (e.g Foot)
+function preexec {
+    print -Pn "\e]0;${(q)1}\e\\"
 }
