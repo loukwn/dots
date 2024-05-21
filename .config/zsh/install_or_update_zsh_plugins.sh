055ba1f0a07d449d6168d775fe5bf344295ad3a6
loukwn <main@loukwn.me>
2024-05-21 18:58:45 +0100

    Added custom plugin install/updater

---
+++ .config/zsh/install_or_update_zsh_plugins.sh
@@ -0,0 +1,20 @@
+declare -A plugins
+
+plugins[zsh-syntax-highlighting]="https://github.com/zsh-users/zsh-syntax-highlighting.git"
+plugins[zsh-autosuggestions]="https://github.com/zsh-users/zsh-autosuggestions.git"
+
+mkdir -p "$ZSH_HOME/plugins"
+cd "$ZSH_HOME/plugins"
+
+for plugin in "${!plugins[@]}"
+do
+    plugin_repo="${plugins[$plugin]}"
+    if [ -d "$plugin" ]; then
+        echo "Attempting to update $plugin..."
+        cd "$plugin"
+        git pull
+    else
+        echo "$plugin does not exist. Installing..."
+        git clone "$plugin_repo"
+fi
+done
--- .config/zsh/plugins.zsh
+++ .config/zsh/plugins.zsh
@@ -1,1 +1,2 @@
 source "$ZSH_HOME"/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+source "$ZSH_HOME"/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh
