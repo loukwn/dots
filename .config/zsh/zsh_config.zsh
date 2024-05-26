ee5a5dd867632cdba426f4a9b75086bb01c17ce5
loukwn <main@loukwn.me>
2024-05-26 14:24:27 +0100

    Enabled compinit on zsh (for Git tab autocompletion)

--- .config/zsh/zsh_config.zsh
+++ .config/zsh/zsh_config.zsh
@@ -5,3 +5,6 @@ HISTSIZE=1000
 # Search history using what already typed (Up and down arrows)
 bindkey '^[[A' up-line-or-search
 bindkey '^[[B' down-line-or-search
+
+# Add completion system. (Git tab-completion etc.)
+autoload -Uz compinit && compinit
